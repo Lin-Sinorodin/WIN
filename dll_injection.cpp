256c21d7704bc41bbf0793ecf8a1b1cf6ae83761
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-29 22:34:43 +0300

    Add DLL injection implementation

---
+++ dll_injection.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "windows.h"
+#include <tchar.h>
+#include <psapi.h>
+
+
+int main()
+{
+    LPCSTR dll_path = "C:\\Users\\hp\\source\\repos\\mydll\\x64\\Release\\mydll.dll";
+    SIZE_T dll_path_len = strlen(dll_path) + 1;  // include null terminator
+    DWORD processID = 15524;
+
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, processID);
+    if (hProcess == NULL) {
+        std::cout << "Failed open process\n";
+        return -1;
+    }
+
+    // allocate memory in remote thread to store dll path
+    LPVOID buffer = VirtualAllocEx(hProcess, NULL, dll_path_len, MEM_COMMIT, PAGE_READWRITE);
+    if (buffer != NULL) {
+        std::cout << "allocated buffer at: " << buffer << std::endl;
+        SIZE_T wrote;
+        if (WriteProcessMemory(hProcess, buffer, dll_path, dll_path_len, &wrote)) {
+            std::cout << "wrote dll path to process memory, bytes: " << wrote << std::endl;
+            HANDLE newThread = CreateRemoteThreadEx(
+                hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, buffer, 0, NULL, NULL
+            );
+            if (newThread == NULL) {
+                std::cout << "failed running dll injection\n";
+            }
+        }
+        VirtualFreeEx(hProcess, buffer, dll_path_len, MEM_DECOMMIT | MEM_RELEASE);
+    }
+    else {
+        std::cout << GetLastError() << std::endl;
+    }
+    
+    CloseHandle(hProcess);
+}
