c12e34f21081c6d8a5f82b7d3cbb99fb4afe8b16
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-29 14:57:53 +0300

    Add files

---
+++ usemydll/usemydll/usemydll.cpp
@@ -0,0 +1,7 @@
+extern "C" __declspec(dllimport) int DisplayMessage();
+
+int main()
+{
+	DisplayMessage();
+	return 0;
+}
---
+++ usemydll/usemydll/usemydll.h
@@ -0,0 +1,2 @@
+#pragma once
+extern "C" __declspec(dllimport) void Share();
