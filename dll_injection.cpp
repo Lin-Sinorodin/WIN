fcad91f42bbb75e61a3e441e084c4a24f3f31afc
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-30 11:28:01 +0300

    Refactor injection code

--- dll_injection.cpp
+++ dll_injection.cpp
@@ -4,22 +4,21 @@
 #include <psapi.h>
 
 // modified from https://learn.microsoft.com/en-us/windows/win32/psapi/enumerating-all-processes
-void GetProcessName(DWORD processID, TCHAR szProcessName) {
-    szProcessName = TEXT("<unknown>");
+void GetProcessName(DWORD processID, LPWSTR processName) {
     HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
-    if (NULL != hProcess ) {
+    if (NULL != hProcess) {
         HMODULE hModule;
         DWORD cbNeeded;
-        if (EnumProcessModules(hProcess, &hModule, sizeof(hModule), &cbNeeded)) {
-            GetModuleBaseName(hProcess, hModule, szProcessName, sizeof(szProcessName)/sizeof(TCHAR));
+        if (EnumProcessModulesEx(hProcess, &hModule, sizeof(hModule), &cbNeeded, LIST_MODULES_64BIT)) {
+            GetModuleBaseNameW(hProcess, hModule, processName, MAX_PATH);
         }
+        CloseHandle(hProcess);
     }
-    CloseHandle(hProcess);
 }
 
 
 // modified from https://learn.microsoft.com/en-us/windows/win32/psapi/enumerating-all-processes
-DWORD FindProcessByName() {
+DWORD FindProcessByName(LPCWSTR targetName) {
     // Get the list of process identifiers.
     DWORD aProcesses[1024], cbNeeded;
     if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {
@@ -30,52 +29,71 @@ DWORD FindProcessByName() {
     DWORD cProcesses = cbNeeded / sizeof(DWORD);
 
     // Print the name and process identifier for each process.
-    TCHAR szProcessName[MAX_PATH];
     for (int i = 0; i < cProcesses; i++) {
-        if(aProcesses[i] != 0) {
-            GetProcessName(aProcesses[i], szProcessName);
+        if (aProcesses[i] != 0) {
+            WCHAR processName[MAX_PATH] = TEXT("<unknown>");
+            GetProcessName(aProcesses[i], processName);
+            if (wcscmp(processName, targetName) == 0) {
+                _tprintf(TEXT("Found process \"%s\" at PID: %05u\n"), processName, aProcesses[i]);
+                return aProcesses[i];
+            }
         }
     }
+    return -1;
 }
 
 
-int main()
-{
-    LPCSTR dll_path = "C:\\Users\\hp\\source\\repos\\mydll\\x64\\Release\\mydll.dll";
-    SIZE_T dll_path_len = strlen(dll_path) + 1;  // include null terminator
-    DWORD processID = 15524;
+void InjectDLL(LPCWSTR targetProcessName, LPCSTR dllPath) {
+    SIZE_T dllPathLen = strlen(dllPath) + 1;  // include null terminator
 
+    // try to find the PID of the target process
+    DWORD processID = FindProcessByName(targetProcessName);
+    if (processID < 0) {
+        std::cout << "Failed locate target process\n";
+        return;
+    }
+
+    // open a handle to target process
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, processID);
     if (hProcess == NULL) {
         std::cout << "Failed open process\n";
-        return -1;
+        return;
     }
 
     // allocate memory in remote thread to store dll path
-    LPVOID buffer = VirtualAllocEx(hProcess, NULL, dll_path_len, MEM_COMMIT, PAGE_READWRITE);
+    LPVOID buffer = VirtualAllocEx(hProcess, NULL, dllPathLen, MEM_COMMIT, PAGE_READWRITE);
     if (buffer != NULL) {
-        std::cout << "allocated buffer at: " << buffer << std::endl;
-
         // copy dll path to remote process virtual memory address
-        SIZE_T wrote;
-        if (WriteProcessMemory(hProcess, buffer, dll_path, dll_path_len, &wrote)) {
-            std::cout << "wrote dll path to process memory, bytes: " << wrote << std::endl;
-
+        if (WriteProcessMemory(hProcess, buffer, dllPath, dllPathLen, NULL)) {
             // try to create a new thread that loads the injected DLL (and runs its entry function)
             HANDLE newThread = CreateRemoteThreadEx(
-                hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, buffer, 0, NULL, NULL
+                    hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, buffer, 0, NULL, NULL
             );
-            if (newThread == NULL) {
+            if (newThread != NULL) {
+                std::cout << "injected dll: " << dllPath << " to target process: " << processID << std::endl;
+            }
+            else {
                 std::cout << "failed running dll injection\n";
             }
         }
+        else {
+            std::cout << "failed write dll path on remote process, error: " << GetLastError() << std::endl;
+        }
 
         // release all allocated memory on the remote process
-        VirtualFreeEx(hProcess, buffer, dll_path_len, MEM_DECOMMIT | MEM_RELEASE);
+        VirtualFreeEx(hProcess, buffer, dllPathLen, MEM_DECOMMIT | MEM_RELEASE);
     }
     else {
-        std::cout << GetLastError() << std::endl;
+        std::cout << "failed allocate memory on remote process, error: " << GetLastError() << std::endl;
     }
-    
+
     CloseHandle(hProcess);
+}
+
+
+int main() {
+    LPCWSTR targetProcessName = L"notepad.exe";
+    LPCSTR dllPath = "C:\\Users\\hp\\Downloads\\my_awesome_dll.dll";
+    InjectDLL(targetProcessName, dllPath);
+    return 0;
 }
